b841c7338fea859f29789cd3b05004fbdc91284a
Silviu Bajenaru Marcu <silviu.bajenaru@net.utcluj.ro>
2016-12-09 18:51:12 +0200

    Pushed to master config.php

--- application/config/config.php
+++ application/config/config.php
@@ -229,7 +229,7 @@ if (ENVIRONMENT == 'production') {
     $config['send_error_at_email'] = 'nbler.ru';
     $config['base_url'] = 'https://dev4.zntral.net/';
     $config['is_developer_comp'] = 1;
-} else if (ENVIRONMENT =='dev4') {
+} else if (ENVIRONMENT =='f3') {
     $config['log_path'] = '/home/zntral/f3/log/'; // '/home/dev9soft/public_html/tb/'
     $config['sql_queries_to_file'] = '/home/zntral/f3/logs/sql_queries_to_file_';
     $config['document_root'] = '/home/zntral/f3/';
