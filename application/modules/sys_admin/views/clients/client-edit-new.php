16376946358bd24bbcdadd348040c3b32c258d16
margaryankarina <margaryankarine4@gmail.com>
2017-04-07 12:57:31 +0400

    2: Overview Page: Client (system admin view)

--- application/modules/sys_admin/views/clients/client-edit-new.php
+++ application/modules/sys_admin/views/clients/client-edit-new.php
@@ -299,87 +299,87 @@
                         <!-- BEGIN CONTENT BODY -->
                         <div class="page-content">
 
-                            <div class="row clients-overview" style="background-color: #fff">
+                            <div class="row clients-overview">
                                 <div class="col s12 m9 l10">
-                                    <div>
+                                    <div class="row" style="margin-bottom: 0;">
                                         <div id="grid-pinned" class="scrollspy">
                                             <h3 class="header">Pinned</h3>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci dicta, eaque error fugit inventore magni minima perspiciatis quos reiciendis sit tempora tempore, voluptas? Delectus impedit nemo odit sunt vitae!</p>
+                                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci dicta, eaque error fugit inventore magni minima perspiciatis quos reiciendis sit tempora tempore, voluptas? Delectus impedit nemo odit sunt vitae!</p>
                                         </div>
+
 
 
                                         <!-- Grid associations -->
-                                        <div id="grid-associations" class="scrollspy">
-                                            <h3 class="header">Associations</h3>
-                                            <nav class="nav-extended">
-                                                <div class="nav-content">
-                                                    <ul class="tabs tabs-transparent">
-                                                        <li class="tab"><a href="#test1" class="active">Users</a></li>
-                                                        <li class="tab"><a href="#test2">Patients</a></li>
-                                                    </ul>
+                                        <div class="row">
+                                            <div id="grid-associations" class="scrollspy">
+                                                <h3 class="header">Associations</h3>
+                                                <nav class="nav-extended">
+                                                    <div class="nav-content">
+                                                        <ul class="tabs tabs-transparent">
+                                                            <li class="tab"><a href="#test1" class="active">Users</a></li>
+                                                            <li class="tab"><a href="#test2">Patients</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </nav>
+                                                <div id="test1" class="col s12">
+                                                    <div class="table-responsive">
+                                                        <table>
+                                                            <thead>
+                                                            <tr>
+                                                                <th>Name</th>
+                                                                <th>Title</th>
+                                                                <th>Status</th>
+                                                                <th>Created</th>
+                                                            </tr>
+                                                            </thead>
+
+                                                            <tbody>
+                                                            <tr>
+                                                                <td>Alvin</td>
+                                                                <td>Eclair</td>
+                                                                <td>Hi</td>
+                                                                <td>$0.87</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Alan</td>
+                                                                <td>Jellybean</td>
+                                                                <td>Hello</td>
+                                                                <td>$3.76</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Jonathan</td>
+                                                                <td>Lollipop</td>
+                                                                <td>Hi</td>
+                                                                <td>$7.00</td>
+                                                            </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
                                                 </div>
-                                            </nav>
-                                            <div id="test1" class="col s12">
-
-                                                <table>
-                                                    <thead>
-                                                    <tr>
-                                                        <th>Name</th>
-                                                        <th>Title</th>
-                                                        <th>Status</th>
-                                                        <th>Created</th>
-                                                    </tr>
-                                                    </thead>
-
-                                                    <tbody>
-                                                    <tr>
-                                                        <td>Alvin</td>
-                                                        <td>Eclair</td>
-                                                        <td>Hi</td>
-                                                        <td>$0.87</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Alan</td>
-                                                        <td>Jellybean</td>
-                                                        <td>Hello</td>
-                                                        <td>$3.76</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Jonathan</td>
-                                                        <td>Lollipop</td>
-                                                        <td>Hi</td>
-                                                        <td>$7.00</td>
-                                                    </tr>
-                                                    </tbody>
-                                                </table>
-
+                                                <div id="test2" class="col s12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </div>
                                             </div>
-                                            <div id="test2" class="col s12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </div>
                                         </div>
-
 
                                         <!-- Grid History -->
 
                                         <div id="grid-history" class="scrollspy">
-                                            <h3 class="header">History</h3>
+                                            <h3 class="header">Activity</h3>
 
-                                            <button type="button" class="btn btn-default" aria-label="Center Align">View History</button>
-
-                                            <h3><i class="fa fa-comment" aria-hidden="true"></i>Add Comment</h3>
 
                                             <div class="row">
-
-
                                                 <div class="widget-area no-padding blank">
                                                     <div class="status-upload">
-                                                        <form>
-                                                            <textarea placeholder="What are you doing right now?" ></textarea>
-                                                            <ul>
-                                                                <li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Audio"><i class="fa fa-music"></i></a></li>
-                                                                <li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Video"><i class="fa fa-video-camera"></i></a></li>
-                                                                <li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Sound Record"><i class="fa fa-microphone"></i></a></li>
-                                                                <li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Picture"><i class="fa fa-picture-o"></i></a></li>
-                                                            </ul>
+                                                        <form action="#">
+                                                            <div class="area-icon">
+                                                                <textarea placeholder="Add Comment"></textarea>
+                                                                <ul>
+                                                                    <li><a title="" data-toggle="tooltip" data-placement="top" data-original-title="Audio"><i class="fa fa-music"></i></a></li>
+                                                                    <li><a title="" data-toggle="tooltip" data-placement="top" data-original-title="Video"><i class="fa fa-video-camera"></i></a></li>
+                                                                    <li><a title="" data-toggle="tooltip" data-placement="top" data-original-title="Sound Record"><i class="fa fa-microphone"></i></a></li>
+                                                                    <li><a title="" data-toggle="tooltip" data-placement="top" data-original-title="Picture"><i class="fa fa-picture-o"></i></a></li>
+                                                                </ul>
+                                                            </div>
+                                                            <button type="button" class="btn btn-default" aria-label="Center Align">View History</button>
                                                             <button type="submit" class="btn btn-success">Send</button>
                                                         </form>
                                                     </div><!-- Status Upload  -->
@@ -400,11 +400,22 @@
                                                         <div class="collapsible-header"><i class="material-icons">whatshot</i>Third Comment</div>
                                                         <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></div>
                                                     </li>
+                                                    <li>
+                                                        <div class="collapsible-header"><i class="material-icons">filter_drama</i>Fourth Comment</div>
+                                                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="collapsible-header"><i class="material-icons">place</i>Fifth Comment</div>
+                                                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="collapsible-header"><i class="material-icons">whatshot</i>Sixth Comment</div>
+                                                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></div>
+                                                    </li>
                                                 </ul>
                                             </div>
 
                                         </div>
-
                                     </div>
                                 </div>
 
@@ -419,7 +430,7 @@
                                             <ul class="section table-of-contents">
                                                 <li><a href="#grid-pinned" class="active">Pinned</a></li>
                                                 <li><a href="#grid-associations" class="">Associations</a></li>
-                                                <li><a href="#grid-history" class="">History</a></li>
+                                                <li><a href="#grid-history" class="">Activity</a></li>
                                             </ul>
                                         </div>
                                     </div>
@@ -1608,7 +1619,6 @@
                             <li data-dismiss="modal" role="button"><a href="#" onclick="javascript:document.location='<?=base_url()?>sys-admin/clients-view<?=$page_parameters_with_sort?>'">CANCEL</a>  </li>
                             <li class="create-contact-save" data-action="save" role="button"><a href="#"> SAVE</a> </li>
                         </ul>
-
 
                     </div>
                 </div>
