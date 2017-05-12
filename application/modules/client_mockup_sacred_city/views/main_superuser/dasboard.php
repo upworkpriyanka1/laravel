d7e8fc33e4f4ec7e3f49c70ee4031532b1b17a7d
margaryankarina <margaryankarine4@gmail.com>
2017-05-12 19:09:43 +0400

    Dasboard

---
+++ application/modules/client_mockup_sacred_city/views/main_superuser/dasboard.php
@@ -0,0 +1,88 @@
+<?php $ci = &get_instance();
+echo link_tag('/assets/layouts/default/css/custom-client-overview.css');
+?>
+<!-- BEGIN MAIN PAGE -->
+<main>
+    <div class="page-content" id="dashboard">
+        <div class="row">
+            <!-- BEGIN CONTENT -->
+            <div class="page-content-wrapper">
+                <!-- BEGIN CONTENT BODY -->
+                <div class="page-content">
+                    <div class="clearfix"></div>
+                    <div class="col s12">
+                        <div>
+                            <!-- Grid visual demo -->
+                            <div id="grid-intro">
+                                <div class="row">
+                                    <div class="pad-card">
+                                        <div class="cards-container">
+                                            <div class="card darken-1">
+                                                <div class="card-content">
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate esse exercitationem in incidunt ipsam praesentium quae quam reprehenderit. Aut impedit laudantium neque quibusdam voluptatem. Numquam placeat quia tempore unde veritatis?
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur cum deleniti distinctio esse excepturi explicabo ipsum itaque nulla obcaecati omnis optio provident, quas quidem quis ut vel veritatis voluptas!
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, nobis sint? Accusamus, aliquam atque consequuntur eius est impedit incidunt labore molestiae necessitatibus nisi officia quia quidem reiciendis repellat sint? Omnis!
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, nobis sint? Accusamus, aliquam atque consequuntur eius est impedit incidunt labore molestiae necessitatibus nisi officia quia quidem reiciendis repellat sint? Omnis!
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, nobis sint? Accusamus, aliquam atque consequuntur eius est impedit incidunt labore molestiae necessitatibus nisi officia quia quidem reiciendis repellat sint? Omnis!
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, nobis sint? Accusamus, aliquam atque consequuntur eius est impedit incidunt labore molestiae necessitatibus nisi officia quia quidem reiciendis repellat sint? Omnis!
+                                                </div>
+
+                                            </div>
+                                            <div class="card darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card  darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card  darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card  darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card  darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card  darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card  darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card  darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card  darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                            <div class="card darken-1">
+                                                <div class="card-content"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- ./page-conten -->
+            </div>
+            <!-- END CONTAINER ./page-content-wrapper -->
+
+        </div>
+    </div>
+</main>﻿
+<!-- END MAIN PAGE -->
+
