0cf2299b904f8f06799afef6ac64192dfee47486
margaryankarina <margaryankarine4@gmail.com>
2017-07-05 19:07:56 +0400

    New/Modify Client Type

--- application/modules/sys_admin/views/clients/clients-types.php
+++ application/modules/sys_admin/views/clients/clients-types.php
@@ -88,6 +88,77 @@
                         </div>
                     </div>
                 </div>
+                <div class="modal fade" id="edit-clients-type" role="dialog">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Modify Client Type</h4>
+                            </div>
+                            <div class="modal-body">
+                                <!-- BEGIN FORM-->
+                                <form action="<?php echo current_url();?>" method="post" id="client-type-add" class="form-horizontal">
+                                    <div class="form-body">
+                                        <div class="alert alert-danger display-hide">
+                                            <button class="close" data-close="alert"></button> <?= lang('form_error');?>
+                                        </div>
+                                        <div class="alert alert-success display-hide">
+                                            <button class="close" data-close="alert"></button> <?= lang('form_sucess');?>
+                                        </div>
+
+                                        <div class="row">
+                                            <!-- Name-->
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group input-field ">
+                                                        <div class="col-md-12">
+                                                            <i class="material-icons prefix">assignment_ind</i>
+                                                            <input type="text" name="data[name]" id="edit-type-name" class="form-control required_form" maxlength="100" />
+                                                            <label for="edit-type-name" class=""><?php echo lang('name') ?><span class="required">&nbsp;*&nbsp;</span></label>
+                                                        </div>
+                                                    </div>
+                                                    <!-- ./form-group -->
+                                                </div><!-- ./col -->
+                                            </div>
+                                            <!-- Description -->
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group input-field ">
+                                                        <div class="col-md-12">
+                                                            <i class="material-icons prefix">business</i>
+                                                            <textarea id="edit-type-description" name="data[description]" data-required="1"  maxlength="100" class=" form-control required_form materialize-textarea"></textarea>
+
+                                                            <label for="edit-type-description" class="description"><?php echo lang('description') ?><span class="required">&nbsp;*&nbsp;</span></label>
+                                                        </div>
+                                                    </div>
+                                                    <!-- ./form-group -->
+                                                </div><!-- ./col -->
+                                            </div>
+
+
+
+                                        </div> <!-- ./row -->
+
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-actions">
+                                                    <div class="row">
+                                                        <div class="col-md-12 text-right">
+                                                            <button type="button" class="btn btn-type" data-dismiss="modal">CANCEL</button>
+                                                            <button type="button" class="btn btn-type">DELETE </button>
+                                                            <button type="button" class="btn btn-type"><?php echo lang('SAVE');?></button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                                <!-- END FORM-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 				<div class="table-responsive">
 					<table class="table table-striped table-bordered table-hover  order-column" id="client_types">
@@ -104,7 +175,7 @@
 
 									<td><?php echo $row->type_name;?></td>
 									<td><?php echo $row->type_description;?>
-                                        <a href="#" class="icon-type-pen"><i class="material-icons">edit</i></a>
+                                        <a href="#" class="icon-type-pen" data-toggle="modal" data-target="#edit-clients-type"><i class="material-icons">edit</i></a>
                                     </td>
 								</tr>
 								<?php
