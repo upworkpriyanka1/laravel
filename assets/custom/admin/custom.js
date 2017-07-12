189d07fe8667cb0b8849eaf0ad5aca5a6f689356
BBITS <projects.bbits@gmail.com>
2017-07-12 18:13:12 +0530

    title redirection change

--- assets/custom/admin/custom.js
+++ assets/custom/admin/custom.js
@@ -25,53 +25,10 @@ $(document).ready(function() {
 				if(result != '')
 				{
 					//alert('here in if...');
-					window.location.href = BASE_URL + result;
+					//window.location.href = BASE_URL + result;
+					window.location.href = result;
 				}
-				/*if (result.ErrorCode == 0) {
-					$('#span_client_client_name_logo_first').html(result.client.formatted_client_logo_first);
-					$('#span_client_client_name').html(result.client.client_name);
-	
-					if ( result.client.client_owner == "" ) {
-						$('#div_client_client_owner').css("display", "none");
-					} else {
-						$('#div_client_client_owner').css("display", "block");
-						$('#span_client_client_owner').html(result.client.client_owner)
-					}
-	
-					if ( result.client.client_email == "" ) {
-						$('#div_client_client_email').css("display", "none");
-					} else {
-						$('#div_client_client_email').css("display", "block");
-						$('#span_client_client_email').html(result.client.client_email)
-					}
-	
-					if ( result.client.formatted_client_city == "" ) {
-						$('#div_client_client_city').css("display", "none");
-					} else {
-						$('#div_client_client_city').css("display", "block");
-						$('#span_client_client_city').html(result.client.formatted_client_city)
-					}
-	
-					if ( result.client.formatted_client_type_label == "" ) {
-						$('#div_client_client_type_label').css("display", "none");
-					} else {
-						$('#div_client_client_type_label').css("display", "block");
-						$('#span_client_client_type_label').html(result.client.formatted_client_type_label)
-					}
-	
-					if ( result.client.formatted_client_address == "" ) {
-						$('#div_client_client_address1').css("display", "none");
-					} else {
-						$('#div_client_client_address1').css("display", "block");
-						$('#span_client_client_address1').html(result.client.formatted_client_address)
-					}
-					if ( result.client.formatted_client_phone == "" ) {
-						$('#div_client_client_phone').css("display", "none");
-					} else {
-						$('#div_client_client_phone').css("display", "block");
-						$('#span_client_client_phone').html(result.client.formatted_client_phone)
-					}
-				}*/
+				
 			}
 		});
 		
