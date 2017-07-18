4c7e97b7555fbc5c3bef90728c6c2a6569e2c16c
Rinni <upworkrinni@gmail.com>
2017-07-18 18:14:18 +0530

    changes

--- application/modules/owner/controllers/owner.php
+++ application/modules/owner/controllers/owner.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Owner_mockup extends CI_Controller {
+class owner extends CI_Controller {
     public function __construct() {
         parent::__construct();               
 		$this->lang->load('sys_admin');		
