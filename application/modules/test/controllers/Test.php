ce72dbefc789c0ba712ec619666d890d2c6693dc
Rinni <upworkrinni@gmail.com>
2017-07-19 10:35:37 +0530

    changes

--- application/modules/test/controllers/Test.php
+++ application/modules/test/controllers/Test.php
@@ -12,24 +12,26 @@ class Test extends CI_Controller {
 	public function index(){
 		$data['meta_description'] = '';
         $data['menu'] = $this->menu;
+		$data['user'] = $this->user; 
         $data['page'] = 'dashboard'; //page view to load   
         $views=  array('design-mockup/html_topbar','sidebar','design-mockup/page','design-mockup/html_footer');
         $this->layout->view($views, $data);
 	}
 	public function locations_list(){		
 		$data['meta_description']='';
-        $data['menu'] = $this->menu;    
-        $data['page']		='main/location_view';       
+        $data['menu'] = $this->menu;
+		$data['user'] = $this->user; 
+        $data['page'] = 'main/location_view';       
         $views=  array('design-mockup/html_topbar','sidebar','design-mockup/page','design-mockup/html_footer');
         $this->layout->view($views, $data);
     }
 	
 	public function contacts_list(){		
 		$data['meta_description']='';
-        $data['menu'] = $this->menu;         
+        $data['menu'] = $this->menu;
+		$data['user'] = $this->user; 	
         $data['page'] = 'main/contacts_list';        
         $views=  array('design-mockup/html_topbar','sidebar','design-mockup/page','design-mockup/html_footer');
         $this->layout->view($views, $data);
     }	
-}
-?>	
+}	
