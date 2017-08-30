0d03ba1861ec673d9e4237a9ad0200399884bf2c
Rinni <upwo>
2017-08-30 16:17:30 +0530

    undo the changes

--- application/modules/sys_admin/controllers/Sys_admin.php
+++ application/modules/sys_admin/controllers/Sys_admin.php
@@ -1,8 +1,7 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
 class Sys_admin extends CI_Controller {
 
     public function __construct() {
