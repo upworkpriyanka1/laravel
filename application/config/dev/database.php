9f38578b8d3e2282c85aa3d7dfb53680535e6607
websitedesignanddeveloper <websitedesignanddeveloper@gmail.com>
2017-09-05 14:17:36 +0000

    database.php edited online with Bitbucket

--- application/config/dev/database.php
+++ application/config/dev/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
         'dsn'   => '',
         'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'database' => 'zntral',
+        'username' => 'zntral_devM',
+        'password' => 'RA.cvosu=aS1',
+        'database' => 'zntral_devM',
         'dbdriver' => 'mysqli',
         'dbprefix' => '',
         'pconnect' => FALSE,
@@ -94,4 +94,3 @@ $db['default'] = array(
         'failover' => array(),
         'save_queries' => TRUE
 );
-
