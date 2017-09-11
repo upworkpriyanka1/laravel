d9b3b1fdf04894971e2cba1bffd2271338578575
Silviu Bajenaru Marcu <silviu.bajenaru@yahoo.com>
2017-09-11 15:55:17 +0000

    Users_mdl.php edited online with Bitbucket

--- application/models/Users_mdl.php
+++ application/models/Users_mdl.php
@@ -891,13 +891,8 @@ class Users_mdl extends CI_Model
         if ((!empty($limit) and $ci->common_lib->is_positive_integer($limit))) {
             $this->db->limit($limit);
         }
-<<<<<<< HEAD
         $this->db->group_by('uc_group_id');
         $fields_for_select .= ' ' . $additive_fields_for_select;
-=======
-//		$this->db->group_by('uc_group_id');
-        $fields_for_select.= ' ' . $additive_fields_for_select;
->>>>>>> origin/Dev_Sergey
         if (!empty($sort)) {
             $this->db->order_by($sort, ((strtolower($sort_direction) == 'desc' or strtolower($sort_direction) == 'asc') ? $sort_direction : ''));
         }
