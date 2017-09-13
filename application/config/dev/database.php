d262e38c770781c26d46b0de9184111b3866188b
Silviu Bajenaru Marcu <silviu.bajenaru@yahoo.com>
2017-09-13 06:12:24 +0000

    Database fix.

--- application/config/dev/database.php
+++ application/config/dev/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
         'dsn'   => '',
         'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'database' => 'zntral',
+        'username' => 'zntral_devM',
+        'password' => 'RA.cvosu=aS1',
+        'database' => 'zntral_devM',
         'dbdriver' => 'mysqli',
         'dbprefix' => '',
         'pconnect' => FALSE,
